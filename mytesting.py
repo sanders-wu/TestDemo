921407e2bce9c05d95c463dee6bb521a65c2fc54
sanders <sanders_945@outlook.com>
2023-09-08 15:46:00 +0800

    第一次提交到Git文件，

---
+++ mytesting.py
@@ -0,0 +1,24 @@
+# 我的python练习模板
+
+# 这是本人的python程序
+
+# 创建日期：2023/9/6/006/22:30
+# 我的python练习模板
+# 这是本人的python程序
+# 创建日期：2022/9/11/20:13
+#year= len(input('请输入生肖年份_'))
+#screenl = len('224123515')
+#if screenl ＜＝ 10:
+#    print("字符串长度小于10")
+#else:
+#    print("字符串长度大于10")
+iputnumber = input('输入任意40以内的整数')
+iput_Num = int(iputnumber)
+if 1 <= iput_Num <= 10:
+    print('数字在10以内')
+elif 11 <= iput_Num <= 20:
+    print('数字在20以内')
+elif 21 <= iput_Num <= 30:
+    print('数字在30以内')
+else:
+    print('数字在40以内')
